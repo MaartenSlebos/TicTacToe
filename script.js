14fe61689004904426ea0fe2c942fd173f792263
MaartenSlebos <maarten.slebos@hotmail.com>
2025-03-25 11:32:33 +0100

    create general structure of gameboard IIFE

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('hello world')
+// The gameboard (IIFE module pattern)
+
+const Gameboard = (function() {
+    let board = ['', '', '', '', '', '', '', '', '']
+    return {
+      placeMarker: function() {
+
+      }
+    };
+  })();
+
+// Player objects (Factory function) 
+
+// The Game Controller (IIFE module pattern)
+
+const gameController = (function() {
+
+    return {
+
+    };
+  })();
